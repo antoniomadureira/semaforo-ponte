2b6364d9f84e2ada138aacf45f2e7178899b9d2d
antoniomadureira <antoniomadureira@gmail.com>
2026-02-20 12:05:07 +0000

    Set base path in Vite config for GitHub Pages
    Add base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/semaforo-ponte/', // Adicionado para que os caminhos funcionem no GitHub Pages
   server: {
     proxy: {
       // Quando o código pedir '/api-apdl', o Vite redireciona para o site real
